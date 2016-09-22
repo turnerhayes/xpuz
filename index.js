988e12d4daf673b4dbb1c114bfcaa3b41fc825d8
Turner Hayes <dev@turnerhayes.com>
2016-09-22 00:50:46 -0400

    updated index documentation

--- index.js
+++ index.js
@@ -8,10 +8,24 @@
  */
 
 exports = module.exports = {
+	/**
+	 * Puzzle file parser constructors
+	 *
+	 * @type object
+	 * @property {function} IPUZ - .ipuz file parser
+	 * @property {function} PUZ - .puz file parser
+	 * @property {function} JPZ - .jpz file parser
+	 */
 	Parsers: {
 		IPUZ: require('./parsers/ipuz'),
 		PUZ: require('./parsers/puz'),
 		JPZ: require('./parsers/jpz')
 	},
+
+	/**
+	 * Puzzle object constructor
+	 *
+	 * @type function
+	 */
 	Puzzle: require('./lib/puzzle'),
 };
